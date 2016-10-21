8ade06d16374c9c2aee9767c4bc1e5ec80821740
Mohamed ali BOUDLAL <boudlalmohamedali@gmail.com>
2016-10-21 23:24:23 +0200

    ajout des twigs et quelques liens des pages layout et index

--- src/FrontOfficeBundle/Controller/DefaultController.php
+++ src/FrontOfficeBundle/Controller/DefaultController.php
@@ -10,4 +10,59 @@ class DefaultController extends Controller
     {
         return $this->render('FrontOfficeBundle:Default:index.html.twig');
     }
+
+    public function specialofferAction()
+    {
+    	return $this->render('FrontOfficeBundle:Default:specialoffer.html.twig');
+    }
+
+    public function nosservicesAction()
+    {
+    	return $this->render('FrontOfficeBundle:Default:nos_services.html.twig');
+    }
+
+    public function contactAction()
+    {
+    	return $this->render('FrontOfficeBundle:Default:contact.html.twig');
+    }
+
+    public function produitsAction()
+    {
+    	return $this->render('FrontOfficeBundle:Default:produits.html.twig');
+    }
+
+    public function camparerAction()
+    {
+    	return $this->render('FrontOfficeBundle:Default:camparer.html.twig');
+    }
+
+    public function passeoublierAction()
+    {
+    	return $this->render('FrontOfficeBundle:Default:passeoublier.html.twig');
+    }
+
+    public function mentionslegalesAction()
+    {
+    	return $this->render('FrontOfficeBundle:Default:mentionslegales.html.twig');
+    }
+
+    public function loginAction()
+    {
+    	return $this->render('FrontOfficeBundle:Default:login.html.twig');
+    }
+
+    public function detailproduitAction()
+    {
+    	return $this->render('FrontOfficeBundle:Default:detailproduit.html.twig');
+    }
+
+    public function resumerproduitAction()
+    {
+    	return $this->render('FrontOfficeBundle:Default:resumerproduit.html.twig');
+    }
+
+    public function inscriptionAction()
+    {
+    	return $this->render('FrontOfficeBundle:Default:inscription.html.twig');
+    }
 }
